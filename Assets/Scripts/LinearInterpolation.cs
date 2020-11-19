1a4e6e203223545082b36fbade86ec91c61f96fb
Klart333 <56590003+Klart333@users.noreply.github.com>
2020-11-19 22:45:42 +0100

    g

---
+++ Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 goalPos;
+
+    [SerializeField]
+    private float curveAmount;
+
+    [SerializeField]
+    private float speed;
+
+    private float time = 0;
+    private Vector3 startPos;
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+    private void Update()
+    {
+        Vector3 curvePos = new Vector3(goalPos.x * curveAmount, goalPos.y, (startPos.z + (goalPos.z - startPos.z) / 2));
+
+        time += (Time.deltaTime / 10) * speed;
+        time = Mathf.Clamp01(time);
+        transform.position = Mathf.Pow(1 - time, 2) * startPos + 2 * (1 - time) * time * curvePos + Mathf.Pow(time, 2) * goalPos;
+    }
+}
